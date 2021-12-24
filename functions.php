10eb0f82c57ea3df21a7253bdac4a82724b1e4c0
iamalexneumann <iamalexneumann@yandex.ru>
2021-12-24 12:21:46 +0300

    Исправляет фильтрацию защиты от XSS в функции cut_text()

--- functions.php
+++ functions.php
@@ -2,9 +2,9 @@
 function cut_text(string $input_string, int $characters_count = 300): string
 {
     if (strlen($input_string) < $characters_count) {
-        $output_string = "<p>htmlspecialchars($input_string)</p>";
+        $output_string = '<p>' . htmlspecialchars($input_string) . '</p>';
     } else {
-        $input_string_array = explode(' ', htmlspecialchars($input_string));
+        $input_string_array = explode(' ', $input_string);
         $result_array = [];
         $result_characters_count = mb_strlen($input_string_array[0]) + 1;
         for ($i = 0; $result_characters_count <= $characters_count; $i += 1) {
@@ -12,7 +12,7 @@ function cut_text(string $input_string, int $characters_count = 300): string
             $result_characters_count += mb_strlen($input_string_array[$i + 1]) + 1;
         }
         $output_string = implode(' ', $result_array);
-        $output_string = "<p>$output_string...</p>";
+        $output_string = '<p>' . htmlspecialchars($output_string) . '...</p>';
         $output_string .= '<a class="post-text__more-link" href="#">Читать далее</a>';
     }
     return $output_string;
