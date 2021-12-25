5ac97fb9fd0d4c762c24b1d62128eda4c03f6b1e
iamalexneumann <iamalexneumann@yandex.ru>
2021-12-25 11:52:44 +0300

    добавляет неработающий ужас

--- functions.php
+++ functions.php
@@ -1,4 +1,8 @@
 <?php
+
+date_default_timezone_set('Europe/Moscow');
+setlocale(LC_ALL, 'ru_RU');
+
 function cut_text(string $input_string, int $characters_count = 300): string
 {
     if (strlen($input_string) < $characters_count) {
@@ -18,42 +22,62 @@ function cut_text(string $input_string, int $characters_count = 300): string
     return $output_string;
 }
 
-date_default_timezone_set('Europe/Moscow');
-setlocale(LC_ALL, 'ru_RU');
-
 function format_date(string $input_date): string
 {
-    $date_now = date_create();
-    $date_diff = date_diff(date_create($input_date), $date_now);
+    $post_date = date_create($input_date);
+    $minute_interval = date_interval_create_from_date_string('60 minutes');
+    $hour_interval = date_interval_create_from_date_string('24 hours');
+    $day_interval = date_interval_create_from_date_string('7 days');
+    $week_interval = date_interval_create_from_date_string('35 days');
 
-    if(date_interval_format($date_diff, '%m') >= 1) {
-        if (date_interval_format($date_diff, '%a') >= 35) {
-            $time_count = date_interval_format($date_diff, '%m');
-            $time_text = get_noun_plural_form($time_count, 'месяц', 'месяца', 'месяцев');
-            return "$time_count $time_text назад";
-        }
+    if (date_diff($post_date, $week_interval)) {
+        echo $post_date;
     }
-    if(date_interval_format($date_diff, '%a') >= 1) {
-        $time_count = date_interval_format($date_diff, '%a');
-        if ($time_count < 7) {
-            $time_text = get_noun_plural_form($time_count, 'день', 'дня', 'дней');
-            return "$time_count $time_text назад";
-        } elseif ($time_count < 35) {
-            $time_count = ceil($time_count / 7);
-            $time_text = get_noun_plural_form($time_count, 'неделя', 'недели', 'недель');
-            return "$time_count $time_text назад";
-        }
-    }
-    if(date_interval_format($date_diff, '%h') >= 1) {
-        $time_count = date_interval_format($date_diff, '%h');
-        $time_text = get_noun_plural_form($time_count, 'час', 'часа', 'часов');
-        return "$time_count $time_text назад";
-    }
-    if(date_interval_format($date_diff, '%i') >= 1) {
-        $time_count = date_interval_format($date_diff, '%i');
-        $time_text = get_noun_plural_form($time_count, 'минута', 'минуты', 'минут');
-        return "$time_count $time_text назад";
-    }
-
-    return "менее минуты назад";
+//    $date_now = new DateTime('now');
+//    $date_post = $input_date;
+//    print_r($date_now);
+//    print_r($date_post);
+//    $date_diff = date_diff(date_create($input_date), $date_now);
+//    print_r($date_diff);
+//    echo strtotime('7 days');
+//    if ($date_diff < strtotime('7 days')) {
+//        echo 'kek';
+//    }
 }
+
+//function format_date(string $input_date): string
+//{
+//    $date_now = date_create();
+//    $date_diff = date_diff(date_create($input_date), $date_now);
+//
+//    if(date_interval_format($date_diff, '%m') >= 1) {
+//        if (date_interval_format($date_diff, '%a') >= 35) {
+//            $time_count = date_interval_format($date_diff, '%m');
+//            $time_text = get_noun_plural_form($time_count, 'месяц', 'месяца', 'месяцев');
+//            return "$time_count $time_text назад";
+//        }
+//    }
+//    if(date_interval_format($date_diff, '%a') >= 1) {
+//        $time_count = date_interval_format($date_diff, '%a');
+//        if ($time_count < 7) {
+//            $time_text = get_noun_plural_form($time_count, 'день', 'дня', 'дней');
+//            return "$time_count $time_text назад";
+//        } elseif ($time_count < 35) {
+//            $time_count = ceil($time_count / 7);
+//            $time_text = get_noun_plural_form($time_count, 'неделя', 'недели', 'недель');
+//            return "$time_count $time_text назад";
+//        }
+//    }
+//    if(date_interval_format($date_diff, '%h') >= 1) {
+//        $time_count = date_interval_format($date_diff, '%h');
+//        $time_text = get_noun_plural_form($time_count, 'час', 'часа', 'часов');
+//        return "$time_count $time_text назад";
+//    }
+//    if(date_interval_format($date_diff, '%i') >= 1) {
+//        $time_count = date_interval_format($date_diff, '%i');
+//        $time_text = get_noun_plural_form($time_count, 'минута', 'минуты', 'минут');
+//        return "$time_count $time_text назад";
+//    }
+//
+//    return "менее минуты назад";
+//}
