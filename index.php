a8054ad6eb8b5902e4c0fa5e30471c1f0a8d2ab0
iamalexneumann <iamalexneumann@yandex.ru>
2021-12-26 12:08:16 +0300

    Добавляет в init.php подключение файлов

--- index.php
+++ index.php
@@ -1,8 +1,5 @@
 <?php
 require_once('init.php');
-require_once('helpers.php');
-require_once('functions.php');
-require_once('data.php');
 
 $page_content = include_template('main.php', [
     'data_posts' => $data_posts,
--- init.php
+++ init.php
@@ -1,3 +1,6 @@
 <?php
 date_default_timezone_set('Europe/Moscow');
 setlocale(LC_ALL, 'ru_RU');
+require_once('helpers.php');
+require_once('functions.php');
+require_once('data.php');
